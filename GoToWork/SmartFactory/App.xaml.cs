a01cf7d9267c197e5b5ecc15bd45612f4c19772b
PARK KWANHO <yujakinasakoon@gmail.com>
2025-08-31 22:30:51 +0900

    Initial Commit

---
+++ GoToWork/SmartFactory/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using SmartFactory.ViewModels;
+using SmartFactory.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartFactory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var viewModel = new MainViewModel();
+            var view = new MainView
+            {
+                DataContext = viewModel,
+            };
+            view.Show();
+        }
+    }
+
+}
